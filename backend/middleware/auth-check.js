7c9f231e0920433c5b2e624258e3baf682cee604
YuriyGnativ <hnativ.yuriy@gmail.com>
2022-01-02 15:12:17 +0200

    removed unused stuff 0

--- backend/middleware/auth-check.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-  const token = req?.headers?.authorization?.split(" ")[1];
-  console.log("token check");
-  // console.log(token);
-  // console.log(req.headers);
-  next();
-  // token ? next({ token }) : next({ token: null });
-};
